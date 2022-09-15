aa305ce9264257e100f89f6fe04a69f485b0b1df
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2022-09-15 10:05:49 +0100

    java

--- src/Traffic_Light/traffic_light.java
+++ src/Traffic_Light/traffic_light.java
@@ -1,2 +1,20 @@
-package Traffic_Light;public class traffic_light {
+package Traffic_Light;
+import java.util.Objects;
+
+public class traffic_light {
+
+        public static void main(String[] args) {
+            String str ="red";
+            System.out.println(updateLight(str).equals("green"));
+        }
+        public static String updateLight(String current) {
+            String [] arr = {"green", "yellow", "red"};
+            for (int i = 0; i < arr.length; i++) {
+                if (Objects.equals(current, arr[arr.length - 1])) { return arr[0];}
+                else if (Objects.equals(current, arr[i])) {
+                    return arr[i+1];
+                }
+            }
+            return "";
+        }
 }
